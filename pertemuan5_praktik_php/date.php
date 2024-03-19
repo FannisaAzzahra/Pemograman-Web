4beae1fbb2419c962f580b4cc65c29bce79b4ad7
FannisaAzzahra <fannisaazzahra1414@gmail.com>
2024-03-19 18:53:47 +0700

    Pertemuan 5 | Date and Time

---
+++ pertemuan5_praktik_php/date.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Date</title>
+</head>
+
+<body>
+    <h3> Date </h3>
+    <?php
+    echo "Todat is " . date("Y/m/d") . "<br>";
+    echo "Todat is " . date("Y.m.d") . "<br>";
+    echo "Todat is " . date("Y-m-d") . "<br>";
+    echo "Todat is " . date("l");
+    ?>
+</body>
+
+</html>
