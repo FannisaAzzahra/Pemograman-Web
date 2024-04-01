64ebd589fb35bd457b9acb312689510829c8e40f
FannisaAzzahra <fannisaazzahra1414@gmail.com>
2024-04-01 08:54:32 +0700

    Pertemuan 7 (Prak 2) | Function empty

---
+++ pertemuan7/empty.php
@@ -0,0 +1,15 @@
+<?php
+
+//$myArray = array();
+//if (empty($myArray)){
+//    echo "Arrat tidak terdefinisi atau kosong";
+//} else {
+//    echo "Arrat terdefinisi atau tidak kosong";
+//}
+
+//2
+if (empty($nonExistentvar)){
+    echo "Arrat tidak terdefinisi atau kosong";
+} else {
+    echo "Arrat terdefinisi atau tidak kosong";
+}
