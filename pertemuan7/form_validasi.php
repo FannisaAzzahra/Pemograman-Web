7a2249cc2180757896947ee8aa26e04b318ba495
FannisaAzzahra <fannisaazzahra1414@gmail.com>
2024-04-01 14:52:04 +0700

    Pertemuan 7 (Prak 7) | Validasi Form

---
+++ pertemuan7/form_validasi.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Form Input dengan Validasi</title>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+</head>
+<body>
+<h1>Form Input dengan Validasi</h1>
+<form id="myForm">
+    <label for="nama">Nama:</label>
+    <input type="text" id="nama" name="nama" >
+    <span id="nama-error" style="color: red;"></span>
+    <br><br>
+
+    <label for="email">Email:</label>
+    <input type="email" id="email" name="email">
+    <span id="email-error" style="color: red;"></span>
+    <br><br>
+
+    <label for="password">Password:</label>
+    <input type="password" id="password" name="password">
+    <span id="password-error" style="color: red;"></span>
+    <br><br>
+
+    <input type="submit" value="Submit">
+</form>
+
+<div id="hasil"></div>
+
+<script>
+    $(document).ready(function () {
+        $("#myForm").submit(function (event) {
+            var nama = $("#nama").val();
+            var email = $("#email").val();
+            var password = $("#password").val();
+            var valid = true;
+
+            if (nama === "") {
+                $("#nama-error").text("Nama harus diisi.")
+                valid = false;
+            } else {
+                $("#nama-error").text("");
+            }
+
+            if (email === "") {
+                $("#email-error").text("Email harus diisi.")
+                valid = false;
+            } else {
+                $('#email-error').text("");
+            }
+
+            if (password.length < 8) {
+                $("#password-error").text("Password harus minimal 8 karakter.")
+                valid = false;
+            } else {
+                $('#password-error').text("");
+            }
+
+            if (!valid) {
+                event.preventDefault();
+            } else {
+                const formData = $("#myForm").serialize();
+
+                $.ajax({
+                    url: 'proses_validasi.php',
+                    type: 'POST',
+                    data: formData,
+                    success: function (res) {
+                        $("#hasil").html(res);
+                    }
+                })
+            }
+        });
+    });
+</script>
+</body>
+</html>
