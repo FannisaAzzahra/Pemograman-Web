6a248f73ac62f1ed67472f34520a55a019e4f994
FannisaAzzahra <fannisaazzahra1414@gmail.com>
2024-04-03 07:28:29 +0700

    Pertemuan 8 (Prak 2) | Multi Upload File

---
+++ pertemuan8/proses_upload_gambar.php
@@ -0,0 +1,39 @@
+<?php
+$targetDirectory = "uploads/";
+
+if (!file_exists($targetDirectory)) {
+    mkdir($targetDirectory, 0777, true);
+}
+
+if ($_FILES['files']['name'][0]) {
+    $totalFiles = count($_FILES['files']['name']);
+
+    for ($i = 0; $i < $totalFiles; $i++) {
+        $fileType = $_FILES['files']['type'][$i];
+        $fileSize = $_FILES['files']['size'][$i];
+        $fileName = $_FILES['files']['name'][$i];
+        $targetFile = $targetDirectory . $fileName;
+
+        // Check if the uploaded file is an image
+        if (substr($fileType, 0, 5) == "image") {
+            // Limit the maximum file size to 5 MB
+            $maxFileSize = 5 * 1024 * 1024;
+
+            if ($fileSize <= $maxFileSize) {
+                if (move_uploaded_file($_FILES['files']['tmp_name'][$i], $targetFile)) {
+                    echo "Gambar $fileName berhasil diunggah.<br>";
+                    echo '<img src="' . $targetFile . '" alt="Thumbnail"><br>';
+                } else {
+                    echo "Gagal mengunggah gambar $fileName.<br>";
+                }
+            } else {
+                echo "Gambar $fileName melebihi ukuran maksimum (5 MB).<br>";
+            }
+        } else {
+            echo "File $fileName bukan gambar. File tidak diunggah.<br>";
+        }
+    }
+} else {
+    echo "Tidak ada gambar yang diunggah.";
+}
+?>
